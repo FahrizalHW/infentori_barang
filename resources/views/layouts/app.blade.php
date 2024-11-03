f105de093f96a82c4abd790fce2feecaa5ae822b
FahrizalHW <hutaothebest666@gmail.com>
2024-11-03 13:39:20 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Snapgram</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
